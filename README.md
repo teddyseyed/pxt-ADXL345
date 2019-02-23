c09f2c67791145b6854fbded6c06baadda183a3d
Teddy Seyed <t-tesey@microsoft.com>
2019-02-22 16:25:17 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# pxt-ADXL345
+# MakeCode driver for the ADXL345 (Beta)
+
+Additional documentation on the ADXL345, including registers and more custom settings can be found here:
+
+https://www.analog.com/media/en/technical-documentation/data-sheets/adxl345.pdf
+
+## License
+
+
+
+## Supported targets
+
+* for PXT/arcade
---
+++ registerhelper.ts
@@ -0,0 +1,33 @@
+/**
+ * Write register at the address location
+ */
+function writeRegister(addr: number, reg: number, dat: number): void {
+    let _registerBuffer = pins.createBuffer(2);
+    _registerBuffer[0] = reg;
+    _registerBuffer[1] = dat;
+    pins.i2cWriteBuffer(addr, _registerBuffer);
+}
+
+/**
+ * Read a 8-byte register of the address location
+ */
+function readRegister8(addr: number, reg: number): number {
+    pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+}
+
+/**
+ * Read a (UInt16) 16-byte register of the address location
+ */
+function readRegisterUInt16(addr: number, reg: number): number {
+    pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(addr, NumberFormat.UInt16LE);
+}
+
+/**
+ * Read a (Int16) 16-byte register of the address location
+ */
+function readRegisterInt16(addr: number, reg: number): number {
+    pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(addr, NumberFormat.Int16LE);
+}
